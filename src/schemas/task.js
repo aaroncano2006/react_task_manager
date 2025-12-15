ef48d040e44575e13cac4797742b6021c33ada66
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2025-12-15 19:58:15 +0100

    Task Zod Schema

---
+++ src/schemas/task.js
@@ -0,0 +1,42 @@
+import { z } from "zod";
+
+export const taskSchema = z.object({
+  taskName: z
+    .string()
+    .min(5, { message: "El nom és obligatori" })
+    .min(5, { message: "Longitud mínima de 5 caràcters" }),
+
+  taskCategory: z
+    .string()
+    .enum(["Personal", "Casa", "Feina", "Estudis"], {
+      message: "El valor ha de pertànyer a una de les categories predefinides",
+    }),
+
+  taskDueDate: z.coerce
+    .date()
+    .min(
+      (() => {
+        const avui = new Date();
+        avui.setHours(0, 0, 0, 0);
+        return avui.getDate() + 1;
+      })(),
+      {
+        message: "La data ha de ser com a mínim demà.",
+      }
+    )
+    .transform((data) => {
+      const dia = String(data.getDate()).padStart(2, "0");
+      const mes = String(data.getMonth() + 1).padStart(2, "0");
+      const any = data.getFullYear();
+      return `${dia}/${mes}/${any}`;
+    }),
+
+  taskImportant: z.boolean().optional(),
+
+  taskDescription: z
+    .string()
+    .max(300, {
+      message: "La descripció ha de contenir com a màxim 300 caràcters.",
+    })
+    .optional(),
+});
