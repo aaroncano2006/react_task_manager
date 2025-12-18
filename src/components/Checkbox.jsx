13446c0aef4f83aad884f4415d7b612451015040
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2025-12-18 15:19:18 +0100

    Component Checkbox.jsx creat

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,21 @@
+import { useFormContext } from "react-hook-form";
+
+function Checkbox({bootstrap = null, name, id, children, defaultValue = null}) {
+    const {
+        register,
+        formState: { errors },
+    } = useFormContext();
+
+    return (
+        <div className="d-flex">
+            <input className={bootstrap} name={name} id={id} defaultValue={defaultValue}/>
+
+            <label className="ms-2" htmlFor={id}>
+                {children}
+            </label>
+        </div>
+        
+    );
+}
+
+export default Checkbox;
