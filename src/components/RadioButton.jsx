6e26ab4d6ebbd23616b37f60d786e7c37ef5eef8
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2025-12-18 15:10:44 +0100

    Canvi en la maquetació del component RadioButton.jsx

--- src/components/RadioButton.jsx
+++ src/components/RadioButton.jsx
@@ -8,11 +8,12 @@ function RadioButton({ bootstrap = null, name, id, children, defaultValue }) {
 
   return (
     <div className="d-flex">
-      <label className="me-2" htmlFor={id}>
+      
+      <input className={bootstrap} type="radio" id={id} defaultValue={defaultValue} {...register(name)} />
+
+      <label className="ms-2" htmlFor={id}>
         {children}
       </label>
-
-      <input className={bootstrap} type="radio" id={id} defaultValue={defaultValue} {...register(name)} />
 
       {errors[name] && <p className="text-danger">{errors[name].message}</p>}
     </div>
